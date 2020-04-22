72922d5e0d66aa168f0527391d35ac789e8bf54f
Cathal Lenihan <cathal@gmst.ie>
2020-04-22 12:53:14 +0100

    Fix issue with bubble not rerendering

--- src/Message.js
+++ src/Message.js
@@ -37,16 +37,24 @@ export default class Message extends React.Component {
   shouldComponentUpdate(nextProps) {
     const next = nextProps.currentMessage;
     const current = this.props.currentMessage;
-    return (
-      next.send !== current.send ||
-      next.received !== current.received ||
-      next.pending !== current.pending ||
-      next.createdAt !== current.createdAt ||
-      next.text !== current.text ||
-      next.image !== current.image ||
-      next.video !== current.video
-    );
-  }
+    const { previousMessage, nextMessage } = this.props;
+    const nextPropsMessage = nextProps.nextMessage;
+    const nextPropsPreviousMessage = nextProps.previousMessage;
+    const shouldUpdate = (this.props.shouldUpdateMessage &&
+        this.props.shouldUpdateMessage(this.props, nextProps)) ||
+        false;
+    return (next.sent !== current.sent ||
+        next.received !== current.received ||
+        next.pending !== current.pending ||
+        next.createdAt !== current.createdAt ||
+        next.text !== current.text ||
+        next.image !== current.image ||
+        next.video !== current.video ||
+        next.audio !== current.audio ||
+        previousMessage !== nextPropsPreviousMessage ||
+        nextMessage !== nextPropsMessage ||
+        shouldUpdate);
+}
 
   getInnerComponentProps = () => {
     const { containerStyle, ...props } = this.props;
